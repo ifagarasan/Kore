ffd454e25b7e330e1967db15431416413d94b514
Ioan Fagarasan <raptor@yahoo.com>
2016-05-19 17:50:44 +0100

    Added Acceptance project and kicked off dev of BinaryGrid

---
+++ Kore.Code/Kore.Code.Acceptance/Grid/BidimensionalGridShould.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kore.Code.Grid;
+
+namespace Kore.Code.Acceptance
+{
+    [TestClass]
+    public class BidimensionalGridShould
+    {
+        [TestMethod]
+        public void SetAndRetrieveDataBasedOnTwoDimensions()
+        {
+            BidimensionalGrid<int> grid = new BidimensionalGrid<int>(10, 10);
+
+            for (uint i = 0; i < 10; ++i)
+                for (uint j = 0; j < 10; ++j)
+                {
+                    int currentValue = (int)(i * 10 + j);
+                    grid.SaveValue(i, j, currentValue);
+                    Assert.AreEqual(currentValue, grid.RetrieveValue(i, j));
+                }
+        }
+    }
+}
---
+++ Kore.Code/Kore.Code/Grid/BidimensionalGrid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Kore.Code.Grid
+{
+    public class BidimensionalGrid<T>
+    {
+        public uint Rows { get; private set; }
+        public uint Columns { get; private set; }
+
+        public BidimensionalGrid(uint rows, uint columns)
+        {
+            Rows = rows;
+            Columns = columns;
+        }
+
+        public void SaveValue(uint row, uint column, int currentValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RetrieveValue(uint row, uint column)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
