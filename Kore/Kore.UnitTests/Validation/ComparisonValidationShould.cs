0e58e555264e8ad98f5e3275f91d2af92f989fd4
Ioan Fagarasan <raptor@yahoo.com>
2016-06-13 11:40:53 +0100

    Added comparisonvalidation

---
+++ Kore/Kore.UnitTests/Validation/ComparisonValidationShould.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kore.Comparers;
+using Kore.Exceptions;
+using Kore.Validation;
+
+namespace Kore.UnitTests.Validation
+{
+    [TestClass]
+    public class ComparisonValidationShould
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ComparisonException))]
+        public void ThrowComparisonExceptionWhenExpectedEqualButGotNotEqual()
+        {
+            ComparisonValidation<int>.IsEqualTo(1, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ComparisonException))]
+        public void ThrowComparisonExceptionWhenExpectedLargerButGotEqual()
+        {
+            ComparisonValidation<int>.IsLargerThan(1, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ComparisonException))]
+        public void ThrowComparisonExceptionWhenExpectedLargerButGotSmaller()
+        {
+            ComparisonValidation<int>.IsLargerThan(1, 2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ComparisonException))]
+        public void ThrowComparisonExceptionWhenExpectedSmallerButGotEqual()
+        {
+            ComparisonValidation<int>.IsSmallerThan(1, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ComparisonException))]
+        public void ThrowComparisonExceptionWhenExpectedSmallerButGotLarger()
+        {
+            ComparisonValidation<int>.IsSmallerThan(1, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ComparisonException))]
+        public void ThrowComparisonExceptionWhenExpectedSmallerOrEqualButGotLarger()
+        {
+            ComparisonValidation<int>.IsSmallerThanOrEqualTo(2, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ComparisonException))]
+        public void ThrowComparisonExceptionWhenExpectedLargerOrEqualButGotSmaller()
+        {
+            ComparisonValidation<int>.IsLargerThanOrEqualTo(1, 2);
+        }
+    }
+}
---
+++ Kore/Kore/Validation/ComparisonValidation.cs
@@ -0,0 +1,39 @@
+﻿using Kore.Comparers;
+using Kore.Exceptions;
+using System;
+
+namespace Kore.Validation
+{
+    public class ComparisonValidation<T> where T : IComparable
+    {
+        public static void IsEqualTo(T value1, T value2)
+        {
+            if (!Comparer<T>.Equal(value1, value2))
+                throw new ComparisonException();
+        }
+
+        public static void IsLargerThan(T value1, T value2)
+        {
+            if (!Comparer<T>.LargerThan(value1, value2))
+                throw new ComparisonException();
+        }
+
+        public static void IsSmallerThan(T value1, T value2)
+        {
+            if (!Comparer<T>.SmallerThan(value1, value2))
+                throw new ComparisonException();
+        }
+
+        public static void IsSmallerThanOrEqualTo(T value1, T value2)
+        {
+            if (Comparer<T>.LargerThan(value1, value2))
+                throw new ComparisonException();
+        }
+
+        public static void IsLargerThanOrEqualTo(T value1, T value2)
+        {
+            if (Comparer<T>.SmallerThan(value1, value2))
+                throw new ComparisonException();
+        }
+    }
+}
