ea2420f9d69f018d51558b78eeec37d0b031332f
ifagarasan <raptor_dev@yahoo.com>
2016-06-13 21:14:52 +0100

    Added ObjectValidation

---
+++ Kore/Kore.UnitTests/Validation/ObjectValidationShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kore.Comparers;
+using Kore.Exceptions;
+using Kore.Validation;
+
+namespace Kore.UnitTests.Validation
+{
+    [TestClass]
+    public class ObjectalidationShould
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowArgumentNullExceptionIfNull()
+        {
+            ObjectValidation.IsNotNull(null);
+        }
+    }
+}
---
+++ Kore/Kore/Validation/ObjectValidation.cs
@@ -0,0 +1,16 @@
+﻿using Kore.Comparers;
+using Kore.Exceptions;
+using System;
+using System.CodeDom;
+
+namespace Kore.Validation
+{
+    public class ObjectValidation
+    {
+        public static void IsNotNull(object o, string argumentName=null)
+        {
+            if (o == null)
+                throw new ArgumentNullException(argumentName ?? nameof(o));
+        }
+    }
+}
