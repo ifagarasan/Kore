ace0b69a84a81926da9d529f243ee6b7e3f56e94
ifagarasan <raptor_dev@yahoo.com>
2016-06-16 00:41:06 +0100

    Recreated Kore.Test - not picked up by myget

--- Kore.Test/Kore.Test/Properties/AssemblyInfo.cs
+++ Kore.Test/Kore.Test/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8eb45e18-a8d6-4749-95a3-3d9c79bf03c3")]
+[assembly: Guid("faefc6b7-db54-48d5-91a1-7d682fd28942")]
 
 // Version information for an assembly consists of the following four values:
 //
