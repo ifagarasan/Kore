1aded48532fa2c5b40a9b373d3817b83026bcd20
ifagarasan <raptor_dev@yahoo.com>
2016-06-15 23:25:28 +0100

    Adding Kore.Test

---
+++ Kore.Test/Kore.Test/Util/IOUtil.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Kore.Test.Util
+{
+    public static class IoUtil
+    {
+        public static void EnsureFolderExits(string folder)
+        {
+            if (Directory.Exists((folder)))
+                return;
+
+            Directory.CreateDirectory(folder);
+        }
+
+        public static void EnsureFileExits(string file)
+        {
+            if (File.Exists((file)))
+                return;
+
+            using (var fs = File.Create(file)) { }
+        }
+    }
+}
