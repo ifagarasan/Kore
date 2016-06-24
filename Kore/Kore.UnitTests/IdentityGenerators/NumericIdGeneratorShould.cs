d209388f983a4a6e99095751eabf76673b06dfbc
Ioan Fagarasan <raptor@yahoo.com>
2016-06-24 16:38:23 +0100

    Added IdentityGenerators to Kore

---
+++ Kore/Kore.UnitTests/IdentityGenerators/NumericIdGeneratorShould.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kore.IdentityGenerators;
+
+namespace Kore.UnitTests.IdentityGenerators
+{
+    [TestClass]
+    public class NumericIdGeneratorShould
+    {
+        [TestMethod]
+        public void ReturnOneTheFirstTimeItsCalled()
+        {
+            Assert.AreEqual(1, NumericIdGenerator.Generate());
+        }
+
+        [TestMethod]
+        public void IncrementWithEachCall()
+        {
+            var value = NumericIdGenerator.Generate();
+
+            for (int i = 0; i < 10; ++i)
+                Assert.AreEqual(value++ + 1, NumericIdGenerator.Generate());
+        }
+    }
+}
---
+++ Kore/Kore/IdentityGenerators/NumericIdGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kore.IdentityGenerators
+{
+    public static class NumericIdGenerator
+    {
+        private static long _currentValue = 1;
+
+        public static long Generate()
+        {
+            return _currentValue++;
+        }
+    }
+}
