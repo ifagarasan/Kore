4581b0ba19b33b0dfce3ee0f50edce759e4e9b02
Ioan Fagarasan <raptor@yahoo.com>
2016-06-24 10:46:09 +0100

    Added Kore.IO.Util.NodeIo.RelativePath

---
+++ Kore.IO/Kore.IO.UnitTests/Util/IoNodeUtilShould.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using Kore.Dev.Util;
+using Kore.IO.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using static Kore.IO.Util.IoNode;
+
+namespace Kore.IO.UnitTests.Util
+{
+    [TestClass]
+    public class IoNodeUtilShould
+    {
+        Mock<IKoreIoNodeInfo> mockSourceNodeInfo;
+        Mock<IKoreIoNodeInfo> mockParentNodeInfo;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockSourceNodeInfo = new Mock<IKoreIoNodeInfo>();
+            mockParentNodeInfo = new Mock<IKoreIoNodeInfo>();
+        }
+
+        #region Relative Path
+
+        [TestMethod]
+        public void ReturnRelativePathToTheParentOnRelativePath()
+        {
+            var parent = "C:\\music";
+            var relative = "favorite\\song.mp3";
+            var source = Path.Combine(parent, relative);
+
+            mockSourceNodeInfo.Setup(m => m.FullName).Returns(source);
+            mockParentNodeInfo.Setup(m => m.FullName).Returns(parent);
+
+            var result = RelativePath(mockSourceNodeInfo.Object, mockParentNodeInfo.Object);
+
+            Assert.AreEqual(relative, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ValidateSourceOnRelativePath()
+        {
+            RelativePath(null, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ValidateParentOnRelativePath()
+        {
+            RelativePath(mockSourceNodeInfo.Object, null);
+        }
+
+        [TestMethod]
+        public void ReturnEmptyStringIfSourceIsNotUnderParentOnRelativePath()
+        {
+            var parent = "C:\\favorite\\music\\rock";
+            var source = "C:\\song.mp3";
+
+            mockSourceNodeInfo.Setup(m => m.FullName).Returns(source);
+            mockParentNodeInfo.Setup(m => m.FullName).Returns(parent);
+
+            var result = RelativePath(mockSourceNodeInfo.Object, mockParentNodeInfo.Object);
+
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        #endregion
+    }
+}
---
+++ Kore.IO/Kore.IO/Util/IoNode.cs
@@ -0,0 +1,18 @@
+﻿using static Kore.Validation.ObjectValidation;
+
+namespace Kore.IO.Util
+{
+    public static class IoNode
+    {
+        public static string RelativePath(IKoreIoNodeInfo source, IKoreIoNodeInfo parent)
+        {
+            IsNotNull(source, nameof(source));
+            IsNotNull(parent, nameof(parent));
+
+            var sourcePath = source.FullName;
+            var parentPath = parent.FullName;
+
+            return sourcePath.StartsWith(parentPath) ? sourcePath.Substring(parentPath.Length).TrimStart('\\') : string.Empty;
+        }
+    }
+}
