111e9ca01f05f26b7f8b7e4a38ee40fe8d95591d
Ioan Fagarasan <raptor_dev@bitbucket.org>
2016-09-10 17:41:08 +0100

    Added Author

--- Kore.Input/Properties/AssemblyInfo.cs
+++ Kore.Input/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kore.Input")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Input")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompanyAttribute("Ioan Fagarasan")]
 [assembly: AssemblyProduct("Kore.Input")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
